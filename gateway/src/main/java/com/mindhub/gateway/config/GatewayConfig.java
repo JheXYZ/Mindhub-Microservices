7c4c03a9152308bc866d623b1d87a3fb8f4b1df1
JheX <nicorodriguez242003@gmail.com>
2025-01-27 09:15:24 -0300

    added eureka server and gateway

---
+++ gateway/src/main/java/com/mindhub/gateway/config/GatewayConfig.java
@@ -0,0 +1,26 @@
+package com.mindhub.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@LoadBalancerClient
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouter(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                .route("user-service", r -> r.path("/api/v1/users/**")
+                        .uri("lb://user-service"))
+                .route("product-service", r -> r.path("/api/v1/products/**")
+                        .uri("lb://product-service"))
+                .route("order-service", r -> r.path("/api/v1/orders/**")
+                        .uri("lb://order-service"))
+                .route("order-item-service", r -> r.path("/api/v1/order-items/**")
+                        .uri("lb://order-service"))
+                .build();
+    }
+}
